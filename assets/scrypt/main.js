855c7977fc8e22de1071bbe580ae331c89941569
mayu.hiruta <k.khg42.206@gmail.com>
2024-05-25 16:53:31 +0900

    240525_mayu.hiruta

---
+++ assets/scrypt/main.js
@@ -0,0 +1,24 @@
+// ハンバーガーメニュー
+var hamburger = $('.header__menu');
+$('.header__button').on('click', function () {
+    hamburger.toggleClass('header__menu-active');
+});
+
+$('a[href^="#"]').click(function () {
+    hamburger.removeClass('header__menu-active');
+});
+
+$(window).on('resize', function () {
+    hamburger.removeClass('header__menu-active');
+});
+
+// カルーセル
+$(function () {
+    $('.gallery__carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+        fade: true,
+    });
+});
